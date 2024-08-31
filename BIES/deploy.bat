7607c65c5aa3dfd80541a2d17f223d68b27d1e3e
Joscalion04 <joscalion04@gmail.com>
2024-08-30 22:49:05 -0600

    deploy4

--- BIES/deploy.bat
+++ BIES/deploy.bat
@@ -1,9 +1,25 @@
 @echo off
 setlocal
 
-:: Solicitar mensaje de commit
+:: Verificar si hay cambios
+git status --porcelain > nul
+if %ERRORLEVEL% neq 0 (
+    echo Error al verificar el estado del repositorio.
+    exit /b 1
+)
+
 set /p commitMessage=Introduce el mensaje de commit: 
 
+:: Verificar si hay cambios para commitear
+for /f "tokens=1" %%A in ('git status --porcelain') do (
+    set "changes=true"
+    goto :foundChanges
+)
+
+echo No hay cambios para commit.
+goto :end
+
+:foundChanges
 :: Añadir todos los cambios
 git add .
 
@@ -13,5 +29,6 @@ git commit -m "%commitMessage%"
 :: Hacer push al repositorio
 git push origin main
 
+:end
 :: Fin del script
 endlocal
