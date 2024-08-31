2ef2f55abf39f4633a487d6a2399648e13d489e1
Joscalion04 <joscalion04@gmail.com>
2024-08-30 22:52:23 -0600

    deploy coreccion

--- BIES/deploy.bat
+++ BIES/deploy.bat
@@ -8,9 +8,8 @@ if %ERRORLEVEL% neq 0 (
     exit /b 1
 )
 
-set /p commitMessage=Introduce el mensaje de commit: 
-
 :: Verificar si hay cambios para commitear
+set "changes=false"
 for /f "tokens=1" %%A in ('git status --porcelain') do (
     set "changes=true"
     goto :foundChanges
@@ -20,6 +19,15 @@ echo No hay cambios para commit.
 goto :end
 
 :foundChanges
+:: Solicitar mensaje de commit
+set /p commitMessage=Introduce el mensaje de commit: 
+
+:: Verificar si el mensaje está vacío
+if "%commitMessage%"=="" (
+    echo El mensaje de commit no puede estar vacío.
+    exit /b 1
+)
+
 :: Añadir todos los cambios
 git add .
 
